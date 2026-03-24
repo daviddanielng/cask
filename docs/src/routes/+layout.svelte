b395662f77e076bcca7c4d4ba8008e624bc3695f
Daniel Enubiaka <danielenubiakamail@gmail.com>
2026-03-24 15:04:30 +0100

    fix: update deployment workflow to use correct artifact path and enhance layout with sidebar component

--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Sidebar from '$lib/components/sidebar.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="flex">
+	<div class="sidebar hidden w-64 md:block">
+		<Sidebar />
+	</div>
+	<div class="content">
+		{@render children()}
+	</div>
+</div>
